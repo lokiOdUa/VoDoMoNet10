8333c4f015b29d31c20ab43fae1cd4e769b8d579
Seva Makhinia <makhinya@gmail.com>
2023-10-01 12:52:44 +0300

    HelloWorld

---
+++ CollectionsUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoDoMoNet10
+{
+    internal class CollectionsUtils
+    {
+        public List<int> GenerateRandomCollection(int N, int min, int max)
+        {
+            Random random = new Random();
+            List<int> collection = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                collection.Add(random.Next(min, max + 1));
+            }
+
+            return collection;
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            if (number <= 3)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0 || number % 3 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void PrintCollection(List<int> collection)
+        {
+            foreach (int number in collection)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoDoMoNet10
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            CollectionsUtils cu = new CollectionsUtils();
+
+            int errorsCount = 0;
+            string errorMessage = "*** ERROR!";
+            string testName = "Initialize me!";
+
+            testName = "TestGenerateRandomCollection";
+//            List<int> coll1 = cu.GenerateRandomCollection(2, 1, 100500);
+            List<int> coll1 = cu.GenerateRandomCollection(3, 1, 1);
+            if (coll1[0] == coll1[1])
+            {
+                Console.WriteLine("{0} in {1} ::: The following values should be different: {2} and {3}", errorMessage, testName, coll1[0], + coll1[1]);
+                errorsCount++; 
+            }
+            if (coll1.Count != 2)
+            {
+                Console.WriteLine("{0} in {1} ::: Array size should be 2, but got: {2}", errorMessage, testName, coll1.Count);
+                errorsCount++;
+            }
+
+            testName = "TestIsPrime";
+            if (cu.IsPrime(2998) == true)
+            {
+                Console.WriteLine("{0} in {1} ::: Number {2} should be prime", errorMessage, testName, 2998);
+                errorsCount++;
+            }
+            if (cu.IsPrime(2999) == false)
+            {
+                Console.WriteLine("{0} in {1} ::: Number {2} should NOT be prime", errorMessage, testName, 2999);
+                errorsCount++;
+            }
+
+            return errorsCount;
+        }
+    }
+}
